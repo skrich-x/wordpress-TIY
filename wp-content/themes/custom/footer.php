7b6f4689e8589d851e09c262b27a555b3aab6219
Kyle Richardson <kylerichardson@Kyles-MacBook-Pro-2.local>
2016-07-30 14:59:32 -0400

    Header && Footer files added

---
+++ wp-content/themes/custom/footer.php
@@ -0,0 +1,102 @@
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 margin-btm-30">
+                <div id="text-2" class="widget footer-col widget_text"><h3 class="widget-title">ABOUT QUICK THEME</h3>
+                    <div class="textwidget">There are many variations of passages of Lorem Ipsum available, but the
+                        majority have suffered alteration in some form, by injected humour, or randomised words which
+                        don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need
+                        to be sure there isn't anything embarrassing hidden in the middle of text
+                    </div>
+                </div>
+                <div id="quick_social_widget-3" class="widget footer-col widget_quick_social_widget">
+                    <ul class="list-inline social-1">
+                        <li><a href="" target=_blank><i title="facebook" class="fa fa-facebook"></i></a></li>
+                        <li><a href="" target=_blank><i title="twitter" class="fa fa-twitter"></i></a></li>
+                        <li><a href="" target=_blank><i title="google-plus" class="fa fa-google-plus"></i></a></li>
+                        <li><a href="" target=_blank><i title="linkedin" class="fa fa-linkedin"></i></a></li>
+                        <li><a href="" target=_blank><i title="pinterest" class="fa fa-pinterest"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-sm-4 margin-btm-30">
+                <div id="text-3" class="widget footer-col widget_text"><h3 class="widget-title">LATEST TWEETS</h3>
+                    <div class="textwidget">
+                        <div class="tweet"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div id="text-4" class="widget footer-col widget_text"><h3 class="widget-title">SUBSCRIBE TO
+                    NEWSLETTER</h3>
+                    <div class="textwidget"><p>There are many variations of passages of Lorem Ipsum available, but the
+                        majority have suffered alteration in some form.</p>
+
+                        <form role="form" class="subscribe-form">
+                            <div class="input-group">
+                                <input type="email" class="form-control" placeholder="Enter email to subscribe">
+                                <span class="input-group-btn">
+  <a class="btn  btn-theme btn-lg" href="#">Ok</a></span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer><!--footer-->
+
+<div class="footer-btm">
+
+    <ul id="menu-footer-menu" class="footer-nav list-inline text-center">
+        <li id="menu-item-562" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-562"><a
+                href="terms-of-use/">Terms of use</a></li>
+        <li id="menu-item-566" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-566"><a
+                href="privacy-and-policy/">Privacy and policy</a></li>
+        <li id="menu-item-563" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-563"><a
+                href="contact-us/">Contact us</a></li>
+        <li id="menu-item-564" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-564"><a
+                href="typography/">Typography</a></li>
+        <li id="menu-item-565" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-565"><a
+                href="sign-in/">Sign In</a></li>
+    </ul>
+    <div class="copyright text-center">
+        Copyright © 2014. Quick Theme. Powered by <a>bootstrap</a> and <a href="#">Design_mylife</a></div>
+</div><!--footer-btm-->
+<!--back to top-->
+<a href="#" class="scrollToTop"><i class="fa fa-angle-up"></i></a>
+<!--back to top end-->
+<!--scripts and plugins -->
+<!--must need plugin jquery-->
+<script src="js/jquery.min.js"></script>
+<!--bootstrap js plugin-->
+<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<!--easing plugin for smooth scroll-->
+<script src="js/jquery.easing.1.3.min.js" type="text/javascript"></script>
+<script src="js/jquery.sticky.js" type="text/javascript"></script>
+<!--on scroll animation-->
+<script src="js/wow.min.js" type="text/javascript"></script>
+<!--easy pie charts-->
+<script src="js/waypoints.min.js" type="text/javascript"></script>
+<!--digit countdown plugin-->
+<script src="js/jquery.counterup.min.js" type="text/javascript"></script>
+<script src="js/easypiechart.js" type="text/javascript"></script>
+<script src="js/jquery.flexslider-min.js" type="text/javascript"></script>
+<script src="js/jquery.mixitup.min.js" type="text/javascript"></script>
+<script src="js/jquery.stellar.min.js" type="text/javascript"></script>
+<!--owl carousel slider-->
+<script src="js/owl.carousel.min.js" type="text/javascript"></script>
+<!--twitter feed-->
+<script src="js/tweetie.min.js" type="text/javascript"></script>
+<!--popup js-->
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+<script src="js/jquery.backstretch.min.js" type="text/javascript"></script>
+<!--valid / working contact form js-->
+<script src="js/contact_me.js" type="text/javascript"></script>
+<script src="js/jqBootstrapValidation.js" type="text/javascript"></script>
+<!--customizable plugin edit according to your needs-->
+<script src="js/custom.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ wp-content/themes/custom/header.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TIY Wordpress Sprint</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- custom css -->
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="screen">
+    <!-- font awesome for icons -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!--flex slider-->
+    <link href="css/flexslider.css" rel="stylesheet" type="text/css" media="screen">
+    <!-- animated css  -->
+    <link href="css/animate.css" rel="stylesheet" type="text/css" media="screen">
+    <!--web fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <!--owl carousel css-->
+    <link href="css/owl.carousel.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="css/owl.theme.css" rel="stylesheet" type="text/css" media="screen">
+    <!--popups css-->
+    <link href="css/magnific-popup.css" rel="stylesheet" type="text/css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body data-spy="scroll" data-offset="80">
+
+
+<section id="home" data-stellar-background-ratio="0.5">
+    <div class="main-flex-slider">
+        <ul class="slides">
+            <li>
+                <figure><img class="img-responsive" src="img/showcase-1.jpg"/>
+                    <figcaption class="parallax-overlay ">
+                        <div class="slider-text animated fadeInDown">
+                            <h1>We are <span class="colored-text">quick</span></h1>
+                            <div class="home-link-btn text-center">
+                                <p class="scrollto"><a class="btn btn-lg btn-dark-2" href="#about">More Details</a></p>
+
+                            </div>
+                        </div>
+                    </figcaption>
+                </figure>
+            </li>
+            <li>
+                <figure><img class="img-responsive" src="img/showcase-2.jpg"/>
+                    <figcaption class="parallax-overlay ">
+                        <div class="slider-text animated fadeInDown">
+                            <h1><span class="colored-text">creative</span> agency</h1>
+                            <div class="home-link-btn text-center">
+                                <p class="scrollto"><a class="btn btn-lg btn-dark-2" href="#about">More Details</a></p>
+
+                            </div>
+                        </div>
+                    </figcaption>
+                </figure>
+            </li>
+            <li>
+                <figure><img class="img-responsive" src="img/showcase-3.jpg"/>
+                    <figcaption class="parallax-overlay ">
+                        <div class="slider-text animated fadeInDown">
+                            <h1>Clean &amp; <span class="colored-text">flexible</span></h1>
+                            <div class="home-link-btn text-center">
+                                <p class="scrollto"><a class="btn btn-lg btn-dark-2" href="#about">More Details</a></p>
+
+                            </div>
+                        </div>
+                    </figcaption>
+                </figure>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<section id="navigation">
+    <div class="navbar navbar-default navbar-static-top " role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand scrollto" href="/">
+                    <img src="img/logo.svg" class="img-responsive"/>
+                </a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul id="menu-primary-menu" class="nav navbar-nav navbar-right scrollto">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#team">Team</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#work">Work</a></li>
+                    <li><a href="#pricing">Pricing</a></li>
+                    <li class="dropdown"><a href="shortcodes/" class="dropdown-toggle" data-toggle="dropdown">Shortcodes<b
+                            class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="shortcodes/latest-postslatest-news/">Latest posts/Latest news</a></li>
+                            <li><a href="shortcodes/our-team/">Our team</a></li>
+                            <li><a href="shortcodes/pricing/">Pricing table</a></li>
+                            <li><a href="shortcodes/portfolio-slider-shortcode/">Portfolio Slider Shortcode</a></li>
+                            <li><a href="shortcodes/portfolio-shortcode/">Portfolio shortcode</a></li>
+                            <li><a href="progress-bar/">Progress bar</a></li>
+                            <li><a href="shortcodes/other-shortcodes/">Other Shortcodes</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="pages/" class="dropdown-toggle" data-toggle="dropdown">Pages<b
+                            class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="blog/">blog</a></li>
+                            <li><a href="pages/portfolio/">portfolio</a></li>
+                            <li><a href="left-sidebar/">Left Sidebar</a></li>
+                            <li><a href="right-sidebar/">Right Sidebar</a></li>
+                            <li><a href="pages/404-error/">404 Error</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#blog">Blog</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div><!--/.container -->
+    </div><!--navbar-default-->
+</section>
